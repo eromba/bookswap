9726e34be150deb20880079b0e4e422657a4c780
Ethan Romba <ethanromba@u.northwestern.edu>
2013-03-25 12:29:52 -0500

    Added Google Analytics Tracking ID to config file

--- application/views/html.php
+++ application/views/html.php
@@ -61,7 +61,7 @@
 
     <?php if (ENVIRONMENT == 'production') { ?>
     <script>
-      var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+      var _gaq=[['_setAccount','<?php print $this->config->item('google_analytics_id'); ?>'],['_trackPageview']];
       (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
       g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
       s.parentNode.insertBefore(g,s)}(document,'script'));
