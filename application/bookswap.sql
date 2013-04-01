7dc0927286a82a835e3d672cc88b1fef8679ab0e
Ethan Romba <ethanromba@u.northwestern.edu>
2013-04-01 15:13:13 -0500

    Cleaned up bookswap.sql
    - Removed SQL comments
    - Removed column / table comments
    - Removed AUTO_INCREMENT settings

--- application/bookswap.sql
+++ application/bookswap.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 3.3.9.2
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Mar 13, 2013 at 09:17 AM
--- Server version: 5.5.10
--- PHP Version: 5.3.6
-
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 
@@ -15,15 +6,6 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
---
--- Database: `bookswap`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `books`
---
 
 CREATE TABLE IF NOT EXISTS `books` (
   `bid` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -46,50 +28,31 @@ CREATE TABLE IF NOT EXISTS `books` (
   `amazon_used_price` decimal(5,2) DEFAULT NULL,
   `amazon_updated` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`bid`),
-  UNIQUE KEY `Unique_Item` (`title`,`edition`,`authors`,`publisher`),
   UNIQUE KEY `ISBN` (`isbn`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT COMMENT='Items includes, but is not limited to isbn-having books.' AUTO_INCREMENT=8302 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `courses`
---
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 CREATE TABLE IF NOT EXISTS `courses` (
   `cid` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `did` int(11) unsigned NOT NULL,
-  `code` varchar(15) NOT NULL COMMENT 'Course_Code known/shown to students',
+  `code` varchar(15) NOT NULL,
   `name` varchar(31) NOT NULL,
-  `bookstore_id` int(11) DEFAULT NULL COMMENT 'value sent to the Bookstore.  It can be NULL because of the Neebo situation where specific courses are never sent to the bookstore..',
+  `bookstore_id` int(11) DEFAULT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`cid`),
   UNIQUE KEY `Department_Course` (`did`,`bookstore_id`),
   KEY `Department_ID` (`did`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3446 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `departments`
---
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `departments` (
   `did` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `tid` int(11) unsigned NOT NULL,
-  `code` varchar(15) NOT NULL COMMENT 'Department_Code shown/known to students',
-  `bookstore_id` int(11) NOT NULL COMMENT 'value sent to the Bookstore',
+  `code` varchar(15) NOT NULL,
+  `bookstore_id` int(11) NOT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`did`),
   UNIQUE KEY `Term_Department` (`tid`,`code`),
   KEY `Term_ID` (`tid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=305 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `posts`
---
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `posts` (
   `pid` int(11) NOT NULL AUTO_INCREMENT,
@@ -102,47 +65,29 @@ CREATE TABLE IF NOT EXISTS `posts` (
   `condition` tinyint(4) NOT NULL,
   `active` tinyint(4) NOT NULL DEFAULT '1',
   PRIMARY KEY (`pid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `sections`
---
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
 
 CREATE TABLE IF NOT EXISTS `sections` (
   `sid` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `cid` int(11) unsigned NOT NULL,
-  `code` varchar(15) NOT NULL COMMENT 'Class_Code known/shown to students',
-  `bookstore_id` varchar(15) NOT NULL COMMENT 'value sent to the Bookstore',
+  `code` varchar(15) NOT NULL,
+  `bookstore_id` varchar(15) NOT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`sid`),
   UNIQUE KEY `Course_Class` (`cid`,`bookstore_id`),
   KEY `Course_ID` (`cid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7622 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `sections_books`
---
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `sections_books` (
-  `weight` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Actually used in sorting, to tell us the importance of Books (based on the order  in which they were received when fetched from bookstore',
+  `weight` int(11) NOT NULL AUTO_INCREMENT,
   `sid` int(11) unsigned NOT NULL,
   `bid` int(11) unsigned DEFAULT NULL,
-  `required_status` tinyint(4) DEFAULT NULL COMMENT 'e.g. "Required", "Recommended", etc.  We grab from Bookstore',
+  `required_status` tinyint(4) DEFAULT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`weight`),
   UNIQUE KEY `Class_Item` (`sid`,`bid`),
   KEY `Item_ID` (`bid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=17476 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `sessions`
---
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `sessions` (
   `session_id` varchar(40) CHARACTER SET utf8 NOT NULL DEFAULT '0',
@@ -154,24 +99,12 @@ CREATE TABLE IF NOT EXISTS `sessions` (
   KEY `last_activity_idx` (`last_activity`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `terms`
---
-
 CREATE TABLE IF NOT EXISTS `terms` (
   `tid` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(15) NOT NULL COMMENT 'Shown in drop down',
+  `name` varchar(15) NOT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`tid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `users` (
   `uid` int(11) NOT NULL AUTO_INCREMENT,
